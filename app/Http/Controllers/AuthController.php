8399b80d169ddb13f6e7ee8a359afae009314d16
chunzz <chiennvph06047@fpt.edu.vn>
2019-08-30 10:32:51 +0700

    update api_passport

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,13 +20,14 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string',
             'email' => 'required|string|email|unique:users',
-            'password' => 'required|string|confirmed',
-            'type' => 'required'
+            'type' => 'required',
+            'password' => 'required|string|confirmed'
+            
         ]);
         $user = new User([
             'name' => $request->name,
             'email' => $request->email,
-            'email' => $request->type,
+            'type' => $request->type,
             'password' => bcrypt($request->password)
         ]);
         $user->save();
@@ -52,17 +53,25 @@ class AuthController extends Controller
             'password' => 'required|string',
             'remember_me' => 'boolean'
         ]);
+
         $credentials = request(['email', 'password']);
+
         if(!Auth::attempt($credentials))
             return response()->json([
                 'message' => 'Unauthorized'
             ], 401);
+
         $user = $request->user();
+
         $tokenResult = $user->createToken('Personal Access Token');
+
         $token = $tokenResult->token;
+
         if ($request->remember_me)
             $token->expires_at = Carbon::now()->addWeeks(1);
+
         $token->save();
+        
         return response()->json([
             'access_token' => $tokenResult->accessToken,
             'token_type' => 'Bearer',
