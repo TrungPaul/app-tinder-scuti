5bf1795285f7a7b35babdb77c6d16221996af5cc
TrungPaul <trungmadrid@gmail.com>
2019-09-05 09:47:09 +0700

    up feature login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use App\User;
+
+class AuthController extends Controller
+{
+    public $successStatus = 200;
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+            'type' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['error'=>$validator->errors()], 401);
+        }
+            $input = $request->all();
+            $input['password'] = bcrypt($input['password']);
+            $user = User::create($input);
+            $success['token'] =  $user->createToken('AppName')->accessToken;
+            return response()->json(['success'=>$success], $this->successStatus);
+    }
+
+    public function login(){
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')]))
+        {
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('AppName')-> accessToken;
+            return response()->json(['success' => $success], $this-> successStatus);
+        } else{
+            return response()->json(['error'=>'Unauthorised'], 401);
+        }
+    }
+}
--- app/User.php
+++ app/User.php
@@ -2,21 +2,25 @@
 
 namespace App;
 
+
+
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
+    protected $table='users';
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','type'
     ];
 
     /**
