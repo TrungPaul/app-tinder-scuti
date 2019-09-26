b99ec68dc2327682d0cd68ede9b32392bbbfbe1e
chun1chunz <chiennvph06047@fpt.edu.vn>
2019-09-26 14:38:37 +0700

    code_sniffer

--- app/Services/CompanyService.php
+++ app/Services/CompanyService.php
@@ -17,16 +17,15 @@ class CompanyService implements CompanyServiceInterface
     public function show($numberload, $request)
     {
         $perpage = $this->perpageCompany($numberload);
-        if (($request->location == null) && ($request->main_business==null))
-      {
-          $result = Company::offset(0)->limit($perpage)->get();
-      } else if (($request->location != null) && ($request->main_business==null)){
-          $result = Company::where('location','like', $request->location)->offset(0)->limit($perpage)->get();
-      } else if (($request->location == null) && ($request->main_business != null)){
-          $result = Company::where('main_business','like', $request->main_business)->offset(0)->limit($perpage)->get();
-      } else{
-          $result = Company::where('location','like', $request->location)->where('main_business','like', $request->main_business)->offset(0)->limit($perpage)->get();
-      }
+        if (($request->location == null) && ($request->main_business==null)) {
+            $result = Company::offset(0)->limit($perpage)->get();
+        } elseif (($request->location != null) && ($request->main_business==null)) {
+            $result = Company::where('location', 'like', $request->location)->offset(0)->limit($perpage)->get();
+        } elseif (($request->location == null) && ($request->main_business != null)) {
+            $result = Company::where('main_business', 'like', $request->main_business)->offset(0)->limit($perpage)->get();
+        } else {
+            $result = Company::where('location', 'like', $request->location)->where('main_business', 'like', $request->main_business)->offset(0)->limit($perpage)->get();
+        }
         $result = $result->load('jds');
         $result = $result->load('contacts');
         return $result;
