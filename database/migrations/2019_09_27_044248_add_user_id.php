dc01ac97b5a12a3216f702d7bd1175aa678b0575
chun1chunz <chiennvph06047@fpt.edu.vn>
2019-09-27 11:47:01 +0700

    user

--- database/migrations/2019_09_27_044248_add_user_id.php
+++ database/migrations/2019_09_27_044248_add_user_id.php
@@ -14,7 +14,7 @@ class AddUserId extends Migration
     public function up()
     {
         Schema::table('companies', function (Blueprint $table) {
-            $table->string('user_id');
+            $table->integer('user_id');
         });
     }
 
