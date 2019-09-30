9cf4dfcad6b114d35a6108d87df8b232b515a984
chun1chunz <chiennvph06047@fpt.edu.vn>
2019-09-30 15:16:18 +0700

    add contact_id

---
+++ database/migrations/2019_09_30_075804_add_contact_id_companies_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddContactIdCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->integer('contact_id');
+            $table->integer('jd_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
